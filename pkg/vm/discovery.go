404db991678ef20ee33d486ffae9781d269410e5
Hamid Salehian <hmd.salehian@gmail.com>
2024-01-02 19:15:46 +0330

    Add compute resource discovery

--- pkg/vm/discovery.go
+++ pkg/vm/discovery.go
@@ -11,19 +11,18 @@ import (
 	"github.com/vmware/govmomi/vim25/mo"
 )
 
-
 type VMInfo struct {
-	Name     string
-	Host     string
-	CPU      int32
-	Memory   int32
-	OS       string
-	IPs      string
-	Status   string
+	Name   string
+	Host   string
+	CPU    int32
+	Memory int32
+	OS     string
+	IPs    string
+	Status string
 }
 
 func Infos(vmo mo.VirtualMachine) *VMInfo {
-	
+
 	return &VMInfo{
 		Name:   vmo.Config.Name,
 		CPU:    vmo.Summary.Config.NumCpu,
@@ -33,7 +32,6 @@ func Infos(vmo mo.VirtualMachine) *VMInfo {
 		Status: string(vmo.Summary.Runtime.PowerState),
 	}
 }
-
 
 type DiscoveryService struct {
 	client *govmomi.Client
@@ -57,22 +55,40 @@ func (d *DiscoveryService) DiscoverDatacenters() ([]*object.Datacenter, error) {
 	return datacenters, nil
 }
 
-// DiscoverDatacenters retrieves a list of datacenters.
-func (d *DiscoveryService) DiscoverHosts(dc *object.Datacenter) ([]*object.HostSystem, error) {
+// DiscoverComputeResource retrieves a list of compute resources.
+func (d *DiscoveryService) DiscoverComputeResource(dc *object.Datacenter) ([]*object.ComputeResource, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	if dc == nil {
+		return nil, fmt.Errorf("the datacenter is nil")
+	}
+
+	finder := find.NewFinder(d.client.Client, true)
+	finder.SetDatacenter(dc)
+	crs, err := finder.ComputeResourceList(ctx, "*")
+	if err != nil {
+		return nil, fmt.Errorf("failed to discover hosts: %v", err)
+	}
+	return crs, err
+}
+
+// DiscoverVMsInsideDC retrieves a list of VMs inside a datacenter.
+func (d *DiscoveryService) DiscoverVMsInsideDC(dc *object.Datacenter) ([]*object.VirtualMachine, error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	finder := find.NewFinder(d.client.Client, true)
 	finder.SetDatacenter(dc)
-	hosts, err := finder.HostSystemList(ctx, "*")
+	vms, err := finder.VirtualMachineList(ctx, "*")
 	if err != nil {
-		return nil, fmt.Errorf("failed to discover hosts: %v", err)
+		return nil, fmt.Errorf("failed to discover VMs inside datacenter %s: %v", dc.Name(), err)
 	}
-	return hosts, nil
+	return vms, nil
 }
 
-// DiscoverVMsInsideDC retrieves a list of VMs inside a datacenter.
-func (d *DiscoveryService) DiscoverVMsInsideDC(dc *object.Datacenter) ([]*object.VirtualMachine, error) {
+// DiscoverVMsInsideHost retrieves a list of VMs inside a host.
+func (d *DiscoveryService) DiscoverVMsInsideHost(dc *object.Datacenter, h *object.HostSystem) ([]*object.VirtualMachine, error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -91,13 +107,13 @@ func (d *DiscoveryService) DiscoverVMInfo(vm *object.VirtualMachine) (*VMInfo, e
 	defer cancel()
 
 	var vmMo mo.VirtualMachine
-	
+
 	err := object.NewVirtualMachine(d.client.Client, vm.Reference()).Properties(ctx, vm.Reference(), []string{"summary", "guest", "config", "runtime"}, &vmMo)
 	if err != nil {
 		return nil, fmt.Errorf("failed to discover VM info for %s: %v", vm.Name(), err)
 	}
 
-	if err != nil { 
+	if err != nil {
 		log.Printf("failed to discover VM Host info for %s: %v", vm.Name(), err)
 	}
 
